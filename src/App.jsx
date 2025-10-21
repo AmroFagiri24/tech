9c079ee0b85a69e780f2ff78f4a48b2f6a3ce97c
AbdullahAldeijy <abdullahaldeijy@gmail.com>
2025-10-22 02:34:46 +0300

    Fix navigation: Add About, Contact, and FAQ sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Layout from '../components/Layout.jsx'
 import Hero from '../components/Hero.jsx'
 import Services from '../components/Services.jsx'
+import About from '../components/About.jsx'
 import Reviews from '../components/Reviews.jsx'
+import Contact from '../components/Contact.jsx'
+import FAQ from '../components/FAQ.jsx'
 import CallToAction from '../components/CallToAction.jsx'
 
 function App() {
@@ -9,7 +12,10 @@ function App() {
     <Layout>
       <Hero />
       <Services />
+      <About />
       <Reviews />
+      <Contact />
+      <FAQ />
       <CallToAction />
     </Layout>
   )
